2cd37801116977d1bed9dee39ea93774631d5f44
Arafat Husayn <hello@arafat.dev>
2022-11-11 02:35:05 +0600

    fork and update for reels only

--- src/content.js
+++ src/content.js
@@ -1,260 +1,52 @@
-const langs = {
-  en: {
-    newsFeedPosts: "News Feed posts",
-    reelsBlock: "Reels and short videos",
-    commentedOn: " commented on a post from ",
-    peopleKnow: "People you may know",
-    suggested: ">Suggested for you<"
-  }
+let __FB_REELS_REMOVER_CONFIG__ = {
+  reels: `[aria-label*="Reels"]`,
+  i_reels: 0,
+  reels_header: `[role="tablist"] div:nth-child(2)`,
+  i_reels_header: 0,
 };
 
-if (langs[document.documentElement.lang] === undefined) {
-  // unknown lang
+function clean() {
+  const config = __FB_REELS_REMOVER_CONFIG__;
+  for (const key in config) {
+    if (typeof config[key] === "string") {
+      const el = document.querySelectorAll(config[key])[
+        config[`i_${key}`] ? parseInt(config[`i_${key}`]) : 0
+      ];
 
+      if (el) {
+        el.style.display = "none";
+      }
+    }
+  }
 }
 
-let ccDebounceTimer = null;
-let definedFeedHolder = false;
-const contentCleaner = (key, isreRun = false, config) => {
-  if (window.pausecc === true) return;
-  console.log(
-    "contentCleaner:v" +
-      (chrome || browser).runtime.getManifest().version +
-      " " +
-      key
-  );
-  try {
-    let feed = null;
-    if (!definedFeedHolder) {
-      //try method 1
-      for (let feedHeader of window.document.querySelectorAll(
-        'h3[dir="auto"]'
-      )) {
-        if (feedHeader.innerText === langs[document.documentElement.lang].newsFeedPosts) {
-          console.log("contentCleaner: try main finder - 1");
-          if (feedHeader.parentNode.children.length > 3) {
-            definedFeedHolder = true;
-            feedHeader.parentNode.classList.add("defined-feed-holder");
-          }
-          break;
-        }
-      }
-    }
-    if (!definedFeedHolder) {
-      //try method 2
-      for (let feedHeader of window.document.querySelectorAll(
-        'h3[dir="auto"]'
-      )) {
-        if (feedHeader.innerText === langs[document.documentElement.lang].newsFeedPosts) {
-          console.log("contentCleaner: try main finder - 2");
-          if (feedHeader.parentNode.children.length === 2) {
-            if (
-              feedHeader.parentNode.children[0].tagName !== "H3" ||
-              feedHeader.parentNode.children[1].tagName !== "DIV"
-            )
-              continue;
-            definedFeedHolder = true;
-            feedHeader.parentNode.children[1].classList.add(
-              "defined-feed-holder"
-            );
-          }
-          break;
-        }
-      }
-    }
-    if (definedFeedHolder) {
-      feed = window.document.getElementsByClassName("defined-feed-holder")[0];
-    }
-
-    if (feed == null) {
-      return console.warn("cannot find facebook feed");
-    }
-    console.log(
-      "contentCleaner:v" +
-        (chrome || browser).runtime.getManifest().version +
-        " " +
-        key +
-        " -clean"
-    );
-    let result = {
-      total: feed.children.length,
-      alreadyRedacted: 0,
-      ignored: 0,
-      opsignored: 0,
-      redacted: {
-        total: 0,
-        reels: 0,
-        ads: 0,
-        suggestions: 0,
-        commentedOn: 0,
-        peopleMayKnow: 0,
-      },
-      monitoring: 0,
-    };
-    for (let elem of feed.children) {
-      if (elem.classList.contains("redact-elem")) {
-        result.alreadyRedacted += 1;
-        continue;
-      }
-      if (elem.classList.contains("no-redact-elem")) {
-        result.ignored += 1;
-        continue;
-      }
-      if (elem.innerHTML.indexOf(langs[document.documentElement.lang].reelsBlock) >= 0) {
-        if (config.reels === true) {
-          elem.classList.add("no-redact-elem");
-          elem.classList.add("no-reels-redact");
-          result.opsignored += 1;
-          continue;
-        }
-        elem.classList.add("redact-elem");
-        elem.classList.add("redact-elem-reels");
-        result.redacted.reels += 1;
-        continue;
-      }
-      if (elem.innerHTML.indexOf(langs[document.documentElement.lang].commentedOn) >= 0) {
-        if (config.commentedOn === true) {
-          elem.classList.add("no-redact-elem");
-          elem.classList.add("no-commentedOn-redact");
-          result.opsignored += 1;
-          continue;
-        }
-        elem.classList.add("redact-elem");
-        elem.classList.add("redact-elem-commentedOn");
-        result.redacted.commentedOn += 1;
-        continue;
-      }
-      if (elem.innerHTML.indexOf(langs[document.documentElement.lang].peopleKnow) >= 0) {
-        if (config.peopleMayKnow === true) {
-          elem.classList.add("no-redact-elem");
-          elem.classList.add("no-peopleMayKnow-redact");
-          result.opsignored += 1;
-          continue;
-        }
-        elem.classList.add("redact-elem");
-        elem.classList.add("redact-elem-peopleMayKnow");
-        result.redacted.peopleMayKnow += 1;
-        continue;
-      }
-      if (
-        elem.innerHTML.indexOf("/ads/about/") > 0 /* ||
-      (elem.innerHTML.indexOf(">p</div>") > 0 &&
-        elem.innerHTML.indexOf(">S</div>") > 0 &&
-        elem.innerHTML.indexOf(">o</div>") > 0 &&
-        elem.innerHTML.indexOf(">n</div>") > 0 &&
-        elem.innerHTML.indexOf(">s</div>") > 0 &&
-        elem.innerHTML.indexOf(">r</div>") > 0 &&
-        elem.innerHTML.indexOf(">e</div>") > 0 &&
-        elem.innerHTML.indexOf(">d</div>") > 0 &&
-
-        elem.innerHTML.indexOf('/groups/') < 0 &&
-        elem.innerHTML.indexOf('/posts/') < 0
-        )*/
-      ) {
-        elem.classList.add("redact-elem");
-        elem.classList.add("redact-elem-ads");
-        result.redacted.ads += 1;
-        continue;
-      }
-      if (elem.innerHTML.indexOf(langs[document.documentElement.lang].suggested) >= 0) {
-        if (config.suggestions === true) {
-          elem.classList.add("no-redact-elem");
-          elem.classList.add("no-suggestions-redact");
-          result.opsignored += 1;
-          continue;
-        }
-        elem.classList.add("redact-elem");
-        elem.classList.add("redact-elem-suggestions");
-        result.redacted.suggestions += 1;
-        continue;
-      }
-      let contentCounter = `${elem.getAttribute("ccount") || ""}`;
-      if (contentCounter == "") contentCounter = "0";
-      contentCounter = Number.parseInt(contentCounter);
-      contentCounter++;
-      elem.setAttribute("ccount", contentCounter);
-      result.monitoring += 1;
-      if (contentCounter >= 20) elem.classList.add("no-redact-elem");
-    }
-    result.redacted.total =
-      result.redacted.reels +
-      result.redacted.ads +
-      result.redacted.suggestions +
-      result.redacted.commentedOn +
-      result.redacted.peopleMayKnow +
-      result.alreadyRedacted;
-    console.log(
-      `contentCleaner: ` +
-        `[opsIgnored: ${result.opsignored}/${result.total}] ` +
-        `[alreadyRedacted: ${result.alreadyRedacted}/${result.total}] ` +
-        `[ignored: ${result.ignored}/${result.total}] ` +
-        `[monitoring: ${result.monitoring}/${result.total}] ` +
-        `[redacted(reels,ads,suggestions,commentedOn,peopleMayKnow): ${result.redacted.reels},${result.redacted.ads},${result.redacted.suggestions},${result.redacted.commentedOn},${result.redacted.peopleMayKnow}/${result.total}] ` +
-        `[cleaned(redacted,ignored,monitoring): ${result.redacted.total},${
-          result.ignored
-        },${result.monitoring}=${
-          result.redacted.total + result.ignored + result.monitoring
-        }/${result.total}] `
-    );
-
-    clearTimeout(ccDebounceTimer);
-    if (isreRun) return;
-    ccDebounceTimer = setTimeout(() => {
-      //if (`${lastAction}` != lastActionKey) return;
-      contentCleaner("re-clear:" + key, true, config);
-    }, 2000);
-  } catch (xcc) {
-    console.error(xcc);
-  }
-};
-
 document.body.onload = () => {
+  clean();
+
+  chrome = chrome || browser;
+
   chrome.storage.sync.get("data", (items) => {
-    let config = (items || {}).data || {};
-    console.log("Known CC Config", config);
-    let contentClearTimer = setInterval(
-      () => contentCleaner("timer", false, config),
-      60000
-    );
-    contentCleaner(undefined, false, config);
+    const config = (items || {}).data || {};
 
-    let lastAction = 0;
-    let debounceTimer = null;
-    document.addEventListener("scroll", function (e) {
-      let now = new Date().getTime();
+    __FB_REELS_REMOVER_CONFIG__ = {
+      ...__FB_REELS_REMOVER_CONFIG__,
+      ...config,
+    };
 
-      if (now - lastAction > 1000) {
-        clearTimeout(debounceTimer);
-        contentCleaner("force", false, config);
-        lastAction = now;
-        return;
-      }
-      if (now - lastAction > 250) {
-        clearTimeout(debounceTimer);
-        //let lastActionKey = `${lastAction}`;
-        debounceTimer = setTimeout(() => {
-          //if (`${lastAction}` != lastActionKey) return;
-          contentCleaner("scroll", false, config);
-          lastAction = now;
-        }, 500);
-      }
-    });
-
-    window.addEventListener("blur", () => {
-      contentCleaner("blur", false, config);
-      clearInterval(contentClearTimer);
-      contentClearTimer = setInterval(
-        () => contentCleaner("timer", false, config),
-        60000
-      );
-    });
-    window.addEventListener("focus", () => {
-      contentCleaner("focus", false, config);
-      clearInterval(contentClearTimer);
-      contentClearTimer = setInterval(
-        () => contentCleaner("timer", false, config),
-        10000
-      );
-    });
+    clean();
   });
 };
+
+const targetNode = document.body;
+
+const config = { childList: true, subtree: true };
+
+const callback = (_mutationList, _observer) => {
+  clean();
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
--- src/popup.js
+++ src/popup.js
@@ -1,28 +1,42 @@
 // popup.js
 
-for (let elem of document.getElementsByClassName("app_version"))
+for (let elem of document.getElementsByClassName("app_version")) {
   elem.innerText = (chrome || browser).runtime.getManifest().version;
+}
 
-const configElems = ["reels", "suggestions", "commentedOn", "peopleMayKnow"];
+const configElems = ["reels", "i_reels", "reels_header", "i_reels_header"];
 
 document.body.onload = () => {
   chrome.storage.sync.get("data", (items) => {
-    let data = (items || {}).data || {};
-    console.log("Restore", data);
+    const data = (items || {}).data || {};
+
+    console.log("Restore", JSON.stringify(data, null, 2));
+
     //if (chrome.runtime.error) return;
-    for (let configElem of configElems)
-      document.getElementById(configElem).checked = data[configElem] === true;
+
+    for (let configElem of configElems) {
+      if (data[configElem]) {
+        document.getElementById(configElem).value = data[configElem];
+      }
+    }
   });
 };
 
 const changeEvent = () => {
-  let d = {};
-  for (let configElem of configElems)
-    d[configElem] = document.getElementById(configElem).checked === true;
+  const d = {};
+
+  for (let configElem of configElems) {
+    d[configElem] = configElem.startsWith("i_")
+      ? parseInt(document.getElementById(configElem).value)
+      : document.getElementById(configElem).value;
+  }
 
   chrome.storage.sync.set({ data: d }, () => {
     //if (chrome.runtime.error) return;
+    console.log(d);
   });
 };
-for (let configElem of configElems)
-  document.getElementById(configElem).onchange = changeEvent;
+
+for (let configElem of configElems) {
+  document.getElementById(configElem).onkeyup = changeEvent;
+}
